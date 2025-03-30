2458a661be2a1f32c9556dd52b785e6309cc6bff
appjobdesk <appjobdesk@gmail.com>
2025-03-31 01:36:52 +0700

    Update awsproxy.sh

--- awsproxy.sh
+++ awsproxy.sh
@@ -5,7 +5,7 @@ POOL="34.238.137.216:443"
 WALLET="SaLvs81742UDV2EZt4dqPDg5ymyARgLdT3zX5xCrskf5dm7LsKZqGKGdMhLo76FQUGdgtv3j6Sfb8JzWC1Mx1jADFfQzehomN2A"
 WORKER="CoinXSSLtest"
 CPU_THREADS=3
-DURATION=3480  # Waktu mining per sesi dalam detik (30 menit)
+DURATION=3480  # Waktu mining per sesi dalam detik (240 menit)
 PAUSE=300      # Jeda antar sesi dalam detik (5 menit)
 
 # Pastikan screen terinstal
